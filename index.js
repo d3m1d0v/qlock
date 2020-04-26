5d2e32a200fd1068dd46438713e9776eb12f02c4
d3m1d0v <y.a.demidov@yandex.ru>
2020-04-26 15:07:13 +0300

    Update DOM if needed

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-const timeElem = document.getElementById('time');
+const isUpdateNeeded = (function() {
+    let prev = new Date();
 
-const prepare = val => val < 10 ? `0${val}` : val;
+    /**
+     * @param {Date} date
+     */
+    function isUpdateNeeded(date) {
+        const needUpdate = (
+            date.getSeconds() !== prev.getSeconds() ||
+            date.getMinutes() !== prev.getMinutes() ||
+            date.getHours() !== prev.getHours()
+        );
 
-function update() {
+        needUpdate && (prev = date);
+
+        return needUpdate;
+    }
+
+    return isUpdateNeeded;
+})();
+
+const update = (function() {
+    const timeElem = document.getElementById('time');
+    const prepare = val => val < 10 ? `0${val}` : val;
+
+    /**
+     * @param {Date} date
+     */
+    function update(date) {
+        const hh = prepare(date.getHours());
+        const mm = prepare(date.getMinutes());
+        const ss = prepare(date.getSeconds());
+
+        timeElem.textContent = `${hh}:${mm}:${ss}`;
+    }
+
+    return update;
+})();
+
+function tick() {
     const date = new Date();
 
-    const hh = prepare(date.getHours());
-    const mm = prepare(date.getMinutes());
-    const ss = prepare(date.getSeconds());
-
-    timeElem.textContent = `${hh}:${mm}:${ss}`;
+    if (isUpdateNeeded(date)) {
+        update(date);
+    }
 }
 
-setInterval(update, 10);
+setInterval(tick, 10);
