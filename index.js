9f520bcb0dc7900ee5f04c10f1fb8767c48f3e27
d3m1d0v <y.a.demidov@yandex.ru>
2020-04-27 23:02:14 +0300

    Qlock size selection

--- index.js
+++ index.js
@@ -15,6 +15,11 @@
         [HOURS]: 24 * 60 * 60 * 1000,
     };
 
+    const SIZES = ['xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl'];
+
+    const [qlockElem] = document.getElementsByClassName('qlock');
+    /** @type {HTMLSelectElement} */
+    const sizeSelect = document.getElementById('qlock-size');
     const timeElem = document.getElementById('time');
     const prepare = val => val < 10 ? `0${val}` : val;
 
@@ -91,7 +96,39 @@
         }
     }
 
-    updateTime(prev);
-    updateProgress(prev);
+    function updateQuery({ size }) {
+        const searchParams = new URLSearchParams(window.location.search);
+
+        size && searchParams.set('size', size);
+
+        window.history.replaceState(window.history.state, '', `?${searchParams.toString()}`);
+    }
+
+    function updateSize(size) {
+        const sizeCns = qlockElem.className.split(' ').filter(cn => cn.startsWith('qlock_size_'));
+        qlockElem.classList.remove(...sizeCns);
+
+        qlockElem.classList.add(`qlock_size_${size}`);
+    }
+
+    sizeSelect.addEventListener('change', function() {
+        const size = this.selectedOptions[0].value;
+
+        updateSize(size);
+        updateQuery({ size });
+    });
+
+    function init() {
+        const querySize = new URLSearchParams(window.location.search).get('size');
+        const size = SIZES.includes(querySize) ? querySize : 'm';
+
+        sizeSelect.value = size;
+        updateSize(size);
+
+        updateTime(prev);
+        updateProgress(prev);
+    }
+
+    init();
     setInterval(tick, 10);
 })();
