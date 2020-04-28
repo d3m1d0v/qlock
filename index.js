f6c0e8e4f20ce4fe36c51b2b739261fc9bb843fc
d3m1d0v <y.a.demidov@yandex.ru>
2020-04-28 12:56:45 +0300

    Dark theme

--- index.js
+++ index.js
@@ -16,10 +16,13 @@
     };
 
     const SIZES = ['xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl'];
+    const THEMES = ['light', 'dark'];
 
     const [qlockElem] = document.getElementsByClassName('qlock');
     /** @type {HTMLSelectElement} */
     const sizeSelect = document.getElementById('qlock-size');
+    /** @type {HTMLSelectElement} */
+    const themeSelect = document.getElementById('qlock-theme');
     const timeElem = document.getElementById('time');
     const prepare = val => val < 10 ? `0${val}` : val;
 
@@ -96,34 +99,61 @@
         }
     }
 
-    function updateQuery({ size }) {
+    function updateQuery({ size, theme }) {
         const searchParams = new URLSearchParams(window.location.search);
 
         size && searchParams.set('size', size);
+        theme && searchParams.set('theme', theme);
 
         window.history.replaceState(window.history.state, '', `?${searchParams.toString()}`);
     }
 
-    function updateSize(size) {
-        const sizeCns = qlockElem.className.split(' ').filter(cn => cn.startsWith('qlock_size_'));
-        qlockElem.classList.remove(...sizeCns);
+    /**
+     * @param {String} mod
+     * @param {String} val
+     */
+    function updateMod(mod, val) {
+        const modCns = qlockElem.className.split(' ').filter(cn => cn.startsWith(`qlock_${mod}`));
+        qlockElem.classList.remove(...modCns);
 
-        qlockElem.classList.add(`qlock_size_${size}`);
+        qlockElem.classList.add(`qlock_${mod}_${val}`);
+    }
+
+    function updateSize(size) {
+        updateMod('size', size);
+    }
+
+    function updateTheme(theme) {
+        updateMod('theme', theme);
     }
 
     sizeSelect.addEventListener('change', function() {
-        const size = this.selectedOptions[0].value;
+        const size = this.value;
 
         updateSize(size);
         updateQuery({ size });
     });
 
+    themeSelect.addEventListener('change', function() {
+        const theme = this.value;
+
+        updateTheme(theme);
+        updateQuery({ theme });
+    });
+
     function init() {
-        const querySize = new URLSearchParams(window.location.search).get('size');
+        const query = new URLSearchParams(window.location.search);
+        const querySize = query.get('size');
+        const queryTheme = query.get('theme');
+
         const size = SIZES.includes(querySize) ? querySize : 'm';
+        const theme = THEMES.includes(queryTheme) ? queryTheme : 'light';
 
         sizeSelect.value = size;
+        themeSelect.value = theme;
+
         updateSize(size);
+        updateTheme(theme);
 
         updateTime(prev);
         updateProgress(prev);
